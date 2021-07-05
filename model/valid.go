5a3cab24cde793fed4535604bcfac3837acff7c8
Jim Schubert <james.schubert@gmail.com>
2021-07-05 14:38:53 -0400

    Some additional godoc comments

--- model/valid.go
+++ model/valid.go
@@ -1,6 +1,12 @@
 package model
 
 //go:generate stringer -type=Valid
+// Valid is represents an enum of valid states for a validations.ValidationResult
+// State names and uses are self-explanatory:
+//	* Success
+// 	* Failure
+//	* Ignored
+//	* Skipped
 type Valid int
 
 const (
