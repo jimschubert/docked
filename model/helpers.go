5994e5e1a49b8ec7f186989099b79cee95405c28
Jim Schubert <james.schubert@gmail.com>
2021-07-10 12:25:25 -0400

    Add comments to helpers/pattern

--- model/helpers.go
+++ model/helpers.go
@@ -7,14 +7,18 @@ import (
 	"github.com/moby/buildkit/frontend/dockerfile/parser"
 )
 
+// StringPtr obtains a pointer of provided string input
 func StringPtr(s string) *string {
 	return &s
 }
 
+// IsBuilderFrom determines if the current node is a FROM command defining an explicit builder syntax
+// Note that this doesn't account for index-based builder patterns.
 func IsBuilderFrom(node *parser.Node) bool {
 	return node.Value == string(commands.From) && strings.Contains(node.Original, " as ")
 }
 
+// IsFinalFrom determines whether the current node is a FROM command without the explicit alias used in named builder pattern
 func IsFinalFrom(node *parser.Node) bool {
 	return node.Value == string(commands.From) && !IsBuilderFrom(node)
 }
--- model/pattern.go
+++ model/pattern.go
@@ -20,6 +20,7 @@ type regexEngine interface {
 
 type goRegexpEngine struct{}
 
+// MatchString determines if value matches against pattern using the go regexp engine.
 func (g goRegexpEngine) MatchString(pattern string, value string) bool {
 	re := regexp.MustCompile(pattern)
 	return re.MatchString(value)
@@ -27,7 +28,8 @@ func (g goRegexpEngine) MatchString(pattern string, value string) bool {
 
 type regexp2Engine struct{}
 
-func (r regexp2Engine) MatchString(pattern string, value string) bool {
+// MatchString determines if value matches against pattern using the regexp2 engine.
+func (r regexp2Engine) MatchString(pattern, value string) bool {
 	re := regexp2.MustCompile(pattern, 0)
 	re.MatchTimeout = time.Second * 1
 	isMatch, _ := re.MatchString(value)
@@ -38,24 +40,32 @@ var (
 	engine regexEngine = goRegexpEngine{}
 )
 
+// Pattern defines the limited string matching interface to perform against a regex pattern
 type Pattern interface {
 	Matches(value string) bool
 }
 
+// StandardPattern is the default implementation abstracting over a pattern for use with the configurable regex engine.
+// Use SetRegexEngine to change the underlying regex library if necessary.
 type StandardPattern struct {
 	pattern string
 }
 
+// Matches dispatches string matching to the configured regex engine
 func (s StandardPattern) Matches(value string) bool {
 	return engine.MatchString(s.pattern, value)
 }
 
+// NewPattern creates a new Pattern instance from the provided value
+// Note that this makes no assertions of the value, and go regexp supports a subset of regex syntax.
+// Use SetRegexEngine to change the underlying regex library if necessary.
 func NewPattern(value string) Pattern {
 	return StandardPattern{
 		pattern: value,
 	}
 }
 
+// SetRegexEngine globally applies the preferred regex library to use
 func SetRegexEngine(e RegexEngine) {
 	if e == RegexpEngine {
 		engine = goRegexpEngine{}
