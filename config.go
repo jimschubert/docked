06c05d0c41497e2a47c2b8d39049347a6cf99b88
Jim Schubert <james.schubert@gmail.com>
2021-08-31 08:22:07 -0400

    chore: reorder config.go for readability

--- config.go
+++ config.go
@@ -8,8 +8,31 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
+// ConfigRuleOverride defines the id-priority override mapping used in a config file
+type ConfigRuleOverride struct {
+	// The rule id to override
+	ID string `yaml:"id"`
+	// The overridden priority
+	Priority *model.Priority `yaml:"priority,omitempty"`
+}
+
 // RuleOverrides is a slice of ConfigRuleOverride. This type allows for simpler definitions and YAML parsing.
 type RuleOverrides []ConfigRuleOverride
+
+// UnmarshalYAML implements the interface necessary to have greater control over deserializing RuleOverrides
+func (r *RuleOverrides) UnmarshalYAML(value *yaml.Node) error {
+	*r = make([]ConfigRuleOverride, 0)
+	var kvp map[string]model.Priority
+	if err := value.Decode(&kvp); err == nil {
+		for key, priority := range kvp {
+			*r = append(*r, ConfigRuleOverride{key, priority.Ptr()})
+		}
+		return nil
+	}
+
+	type raw RuleOverrides
+	return value.Decode((*raw)(r))
+}
 
 // Config represents the YAML config structure exposed to users
 type Config struct {
@@ -32,27 +55,4 @@ func (c *Config) Load(path string) error {
 	}
 
 	return yaml.Unmarshal(b, c)
-}
-
-// ConfigRuleOverride defines the id-priority override mapping used in a config file
-type ConfigRuleOverride struct {
-	// The rule id to override
-	ID string `yaml:"id"`
-	// The overridden priority
-	Priority *model.Priority `yaml:"priority,omitempty"`
-}
-
-// UnmarshalYAML implements the interface necessary to have greater control over deserializing RuleOverrides
-func (r *RuleOverrides) UnmarshalYAML(value *yaml.Node) error {
-	*r = make([]ConfigRuleOverride, 0)
-	var kvp map[string]model.Priority
-	if err := value.Decode(&kvp); err == nil {
-		for key, priority := range kvp {
-			*r = append(*r, ConfigRuleOverride{key, priority.Ptr()})
-		}
-		return nil
-	}
-
-	type raw RuleOverrides
-	return value.Decode((*raw)(r))
 }
