30c4e81d5321a0452ded6899c9b939307998af82
Jim Schubert <james.schubert@gmail.com>
2021-09-15 08:08:27 -0400

    feat: avoid-copy-all rule
    This rule is a recommendation against directives like:
    
    COPY . /
    
    These are potentially dangerous as they could copy files ignored by
    global git rules, or uncommitted files in the working directory. This
    might include env files, gpg signing keys, or others that should not end
    up in an image.

---
+++ model/rules/avoid_copy_all.go
@@ -0,0 +1,31 @@
+package rules
+
+import (
+	"strings"
+
+	"github.com/jimschubert/docked/model"
+	"github.com/jimschubert/docked/model/docker/commands"
+	"github.com/jimschubert/docked/model/validations"
+	"github.com/moby/buildkit/frontend/dockerfile/parser"
+)
+
+func avoidCopyAll() validations.Rule {
+	r := validations.MultiContextRule{
+		Name: "avoid-copy-all",
+		Summary: "Avoid copying entire source directory into image",
+		Details: "Explicitly copying sources helps avoid accidentally persisting secrets or other files that should not be shared.",
+		Priority: model.HighPriority,
+		Commands: []commands.DockerCommand{commands.Copy},
+		Evaluator: func(node *parser.Node, validationContext validations.ValidationContext) model.Valid {
+			if strings.HasPrefix(node.Original, "COPY . ") {
+				return model.Recommendation
+			}
+			return model.Skipped
+		},
+	}
+	return &r
+}
+
+func init() {
+	AddRule(avoidCopyAll())
+}
